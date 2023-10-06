4e0c7fd236a84a36e2a00a8a54d5e8cfbd5f79cf
Alexander Nafasov <sasha.nafasov@gmail.com>
2023-10-07 00:37:06 +0500

    init value for charge trigger

--- utils/GetChargeTrigger.lua
+++ utils/GetChargeTrigger.lua
@@ -2,12 +2,14 @@ local savedWeaponName = ''
 local savedFrequency = 0
 local maxFrequency = 50
 
-local function GetChargeTrigger(weaponName, isTimeDilated, reset)
+local function GetChargeTrigger(weaponName, isTimeDilated, reset, initValue)
     if (savedWeaponName ~= weaponName) then savedFrequency = 0 end
     if (reset) then savedFrequency = 0 return 0 end
 
+    if (initValue and savedFrequency <= 0) then savedFrequency = initValue end
+
     savedWeaponName = weaponName
-    savedFrequency = savedFrequency + 0.2
+    savedFrequency = savedFrequency + 0.3
 
     if (savedFrequency > maxFrequency) then savedFrequency = maxFrequency end
 
