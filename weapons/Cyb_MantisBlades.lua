99e3ff466f12d2c8d0a041114e289592537189e0
Alexander Nafasov <sasha.nafasov@gmail.com>
2023-10-07 00:37:22 +0500

    improved trigger effects

--- weapons/Cyb_MantisBlades.lua
+++ weapons/Cyb_MantisBlades.lua
@@ -7,7 +7,7 @@ local function Weapon(data, name, isAiming)
     data.leftTriggerType = 'Bow'
     data.leftForceTrigger = '(0)(1)(4)(5)'
     data.rightTriggerType = 'Bow'
-    data.rightForceTrigger = '(1)(3)(5)(3)'
+    data.rightForceTrigger = '(1)(3)(7)(3)'
 
     if (state == 0) then
         data.leftTriggerType = 'Resistance'
@@ -16,34 +16,45 @@ local function Weapon(data, name, isAiming)
         data.rightForceTrigger = '(1)(1)'
     end
 
-    if (stamina == 1) then
+    if (stamina == 2) then
         data.leftForceTrigger = '(0)(1)(8)(8)'
-        data.rightForceTrigger = '(1)(5)(8)(8)'
+        data.rightForceTrigger = '(1)(3)(8)(8)'
     end
 
-    if (state == 5 or state == 6) then
+    if (state == 5 or state == 6 or state == 7 or state == 13) then
         data.rightForceTrigger = '(1)(5)(5)(6)'
-        if (stamina == 1) then data.rightForceTrigger = '(1)(6)(8)(8)' end
+        if (stamina == 2) then data.rightForceTrigger = '(1)(6)(8)(8)' end
+    end
+
+    if (state == 10) then
+        data.leftTriggerType = 'Resistance'
+        data.leftForceTrigger = '(5)(1)'
+
+        if (stamina == 2) then data.leftForceTrigger = '(5)(3)' end
+    end
+
+    if (state == 11) then
+        data.rightTriggerType = 'Bow'
+        data.rightForceTrigger = '(0)(6)(1)(5)'
+
+        if (stamina == 2) then data.rightForceTrigger = '(0)(7)(3)(7)' end
     end
 
     if (isAiming) then
-        data.leftTriggerType = 'Resistance'
-        data.leftForceTrigger = '(5)(1)'
-        if (stamina == 1) then data.leftForceTrigger = '(5)(3)' end
-
         data.rightTriggerType = 'Choppy'
-        if (stamina == 1) then data.rightTriggerType = 'Rigid' end
+        if (stamina == 2) then data.rightTriggerType = 'Rigid' end
+    end
 
-        if (state == 14) then
+    if (state == 14 or state == 15) then
+        data.leftTriggerType = 'Resistance'
+        data.leftForceTrigger = '(5)(3)'
+
+        data.rightTriggerType = 'Bow'
+        data.rightForceTrigger = '(2)(7)(2)(6)'
+
+        if (stamina == 2) then
             data.leftForceTrigger = '(5)(5)'
-
-            data.rightTriggerType = 'Resistance'
-            data.rightForceTrigger = '(5)(3)'
-
-            if (stamina == 1) then
-                data.leftForceTrigger = '(5)(8)'
-                data.rightForceTrigger = '(5)(5)'
-            end
+            data.rightForceTrigger = '(5)(5)'
         end
     end
 
--- weapons/Cyb_NanoWires.lua
+++ weapons/Cyb_NanoWires.lua
@@ -8,24 +8,60 @@ local function Weapon(data, name, isAiming)
     local stamina = GetState('Stamina')
     local state = GetState('MeleeWeapon');
 
-    print(state)
+    if (state ~= 6 and state ~= 10 and state ~= 15) then GetChargeTrigger(name, false, true) end
 
-    if (stamina == 1) then
+    if (stamina == 2) then
         data.leftTriggerType = 'Resistance'
-        data.leftForceTrigger = '(1)(5)'
+        data.leftForceTrigger = '(1)(3)'
         data.rightForceTrigger = '(0)(3)(8)(8)'
     end
 
-    if (isAiming) then
-        data.rightTriggerType = 'Bow'
-        data.rightForceTrigger = '(1)(3)(7)(7)'
-        if (stamina == 1) then data.rightForceTrigger = '(0)(3)(8)(8)' end
+    if (state == 6) then
+        local freq = GetChargeTrigger(name, false, false, 8)
+
+        data.rightTriggerType = 'Galloping'
+        data.rightForceTrigger = '(2)(9)(2)(3)('.. freq ..')'
     end
 
-    if (state == 6) then
-      data.rightTriggerType = 'Bow'
-      data.rightForceTrigger = '(1)(5)(7)(7)'
+    if (state == 7 or state == 13) then
+        data.rightTriggerType = 'Bow'
+        data.rightForceTrigger = '(2)(7)(3)(7)'
     end
+
+    if (state == 10) then
+        local freq = GetChargeTrigger(name, false, false, 4)
+
+        data.leftTriggerType = 'Galloping'
+        data.leftForceTrigger = '(2)(9)(1)(2)('.. freq ..')'
+    end
+
+    if (state == 8) then
+        data.rightTriggerType = 'Bow'
+        data.rightForceTrigger = '(1)(3)(7)(7)'
+        if (stamina == 2) then data.rightForceTrigger = '(0)(3)(8)(8)' end
+    end
+
+    if (state == 11) then
+        data.rightTriggerType = 'Bow'
+        data.rightForceTrigger = '(0)(6)(1)(4)'
+    end
+
+    if (state == 12) then
+        data.rightTriggerType = 'Bow'
+        data.rightForceTrigger = '(0)(7)(2)(8)'
+    end
+
+    if (state == 15) then
+        local freq = GetChargeTrigger(name, false, false, 20)
+
+        data.rightTriggerType = 'Galloping'
+        data.rightForceTrigger = '(4)(9)(2)(3)('.. freq ..')'
+    end
+
+    -- if (state == 6) then
+    --   data.rightTriggerType = 'Bow'
+    --   data.rightForceTrigger = '(1)(5)(7)(7)'
+    -- end
 
     if (state == 19) then
       data.leftTriggerType = 'Resistance'
