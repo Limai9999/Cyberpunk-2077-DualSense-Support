05c9570a3d1966a142486225d41df13c404feead
Alexander Nafasov <sasha.nafasov@gmail.com>
2023-12-09 18:29:44 +0500

    improved chainsword trigger effect

--- utils/GetChargeTrigger.lua
+++ utils/GetChargeTrigger.lua
@@ -1,12 +1,21 @@
 local savedWeaponName = ''
 local savedFrequency = 0
 local maxFrequency = 50
+local initComplete = false
+
+local function resetSaved()
+    savedFrequency = 0
+    initComplete = false
+end
 
 local function GetChargeTrigger(weaponName, isTimeDilated, reset, stepValue, initValue, maxValue)
-    if (savedWeaponName ~= weaponName) then savedFrequency = 0 end
-    if (reset) then savedFrequency = 0 return 0 end
+    if (savedWeaponName ~= weaponName) then resetSaved() end
+    if (reset) then resetSaved() return 0 end
 
-    if (initValue and savedFrequency <= 0) then savedFrequency = initValue end
+    if (initValue and savedFrequency <= 0 and not initComplete) then
+        savedFrequency = initValue
+        initComplete = true
+    end
 
     savedWeaponName = weaponName
     savedFrequency = savedFrequency + stepValue
@@ -16,6 +25,8 @@ local function GetChargeTrigger(weaponName, isTimeDilated, reset, stepValue, ini
     else
         if (savedFrequency > maxFrequency) then savedFrequency = maxFrequency end
     end
+
+    if (savedFrequency < 0) then savedFrequency = 0 end
 
     local freq = GetFrequency(math.floor(savedFrequency), isTimeDilated, weaponName)
 
--- weapons/Wea_Chainsword.lua
+++ weapons/Wea_Chainsword.lua
@@ -1,6 +1,8 @@
 local function Weapon(data, name, isAiming, _, dilated, triggerType, isWeaponGlitched, attackSpeed, config)
     data.type = GetText('Gameplay-Items-Item Type-Wea_Chainsword')
 
+    if (not name) then return data end
+
     local state = GetState('MeleeWeapon')
 
     data.leftTriggerType = 'Resistance'
@@ -8,19 +10,28 @@ local function Weapon(data, name, isAiming, _, dilated, triggerType, isWeaponGli
     data.rightTriggerType = 'Bow'
     data.rightForceTrigger = '(0)(3)(5)(5)'
 
-    if (state == 8 or state == 10) then
-        local freq = GetChargeTrigger(name, dilated, false, 0.1, 2, 10)
+    if (state == 8 or state == 10 or state == 7 or state == 15) then
+        if (state == 7) then
+            local freq = GetChargeTrigger(name..'7', dilated, false, 0.4, 5, 20)
 
-        data.leftTriggerType = 'Machine'
-        data.leftForceTrigger = '(1)(9)(1)(2)('.. freq ..')(0)'
+            data.rightTriggerType = 'Machine'
+            data.rightForceTrigger = '(1)(9)(3)(3)('.. freq ..')(0)'
+        elseif (state == 15) then
+            local freq = GetChargeTrigger(name..'15', dilated, false, -1, 30, 30)
+
+            data.rightTriggerType = 'Machine'
+            data.rightForceTrigger = '(1)(9)(4)(4)('.. freq ..')(0)'
+        elseif (state == 8 or state == 10) then
+            local freq = GetChargeTrigger(name..'810', dilated, false, 0.1, 2, 10)
+
+            data.leftTriggerType = 'Machine'
+            data.leftForceTrigger = '(1)(9)(1)(2)('.. freq ..')(0)'
+        end
     else
         GetChargeTrigger(name, dilated, true)
     end
 
-    if (state == 7) then
-        data.rightTriggerType = 'Bow'
-        data.rightForceTrigger = '(0)(4)(7)(3)'
-    elseif (state == 6) then
+    if (state == 6) then
         data.rightTriggerType = 'Bow'
         data.rightForceTrigger = '(0)(6)(7)(7)'
     elseif (state == 13) then
