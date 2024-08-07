5f910eb6f272169860e3a02ee5de2c33b7ed0e39
Alexander Nafasov <sasha.nafasov@gmail.com>
2024-08-08 03:00:39 +0500

    SOS

--- weapons/Wea_TwoHandedClub.lua
+++ weapons/Wea_TwoHandedClub.lua
@@ -1,4 +1,6 @@
 local function Weapon(data, name, isAiming, _, dilated, triggerType, isWeaponGlitched, attackSpeed, config)
+    -- ! SOS: TODO: HELP: If anybody is playing with these weapons, please make a good trigger for them yourself. I think I haven't changed this trigger since the first version of the mod, because I really don't know which trigger will be the best!
+
     data.type = GetText('Gameplay-RPG-Items-Types-Wea_TwoHandedClub')
 
     data.leftTriggerType = 'Choppy'
