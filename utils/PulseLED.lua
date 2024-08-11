be13d8a8c5adb2377e16016b231e010aa3ffe0ba
Alexander Nafasov <sasha.nafasov@gmail.com>
2024-08-12 01:42:43 +0500

    improved pulse led logic

--- utils/PulseLED.lua
+++ utils/PulseLED.lua
@@ -1,66 +1,52 @@
 local direction = 'down'
 
-local savedR = 0
-local savedG = 0
-local savedB = 0
-
-local maxStopSkip = 20
+local savedBrightness = 0
 local stopSkipped = 0
-
 local savedType = ''
 
-local function PulseLED(type, speed, r, g, b, min)
+local function PulseLED(type, speedUnformatted, r, g, b, minPercent, maxStopSkipUnformatted)
+    local maxStopSkip = CalcTimeIndex(maxStopSkipUnformatted)
+    local speed = CalcTimeIndex(speedUnformatted, true)
+
     if (type ~= savedType) then
         direction = 'down'
-        savedR = r
-        savedG = g
-        savedB = b
+        savedBrightness = math.max(r, g, b)
         stopSkipped = 0
     end
 
     savedType = type
 
-    local sum = 0
+    -- Normalize the original color values to maintain the ratio
+    local maxColor = math.max(r, g, b)
+    local normR = r / maxColor
+    local normG = g / maxColor
+    local normB = b / maxColor
 
+    -- Calculate the minimum brightness based on the given percentage
+    local minBrightness = maxColor * (minPercent / 100)
+
+    -- Adjust brightness based on direction
     if (direction == 'down') then
-        savedR = savedR - speed
-        savedG = savedG - speed
-        savedB = savedB - speed
+        savedBrightness = savedBrightness - speed
 
-        if (savedR <= 0) then savedR = 0 end
-        if (savedG <= 0) then savedG = 0 end
-        if (savedB <= 0) then savedB = 0 end
-
-        sum = savedR + savedG + savedB
-
-        if (sum < min) then
-            savedR = 0
-            savedG = 0
-            savedB = 0
+        if (savedBrightness < minBrightness) then
+            savedBrightness = minBrightness
 
             if (stopSkipped < maxStopSkip) then
                 stopSkipped = stopSkipped + 1
-                direction = 'down'
             else
                 stopSkipped = 0
                 direction = 'up'
             end
         end
     elseif (direction == 'up') then
-        savedR = savedR + speed
-        savedG = savedG + speed
-        savedB = savedB + speed
+        savedBrightness = savedBrightness + speed
 
-        local maxedCount = 0
+        if (savedBrightness >= maxColor) then
+            savedBrightness = maxColor
 
-        if (savedR > r) then savedR = r maxedCount = maxedCount + 1 end
-        if (savedG > g) then savedG = g maxedCount = maxedCount + 1 end
-        if (savedB > b) then savedB = b maxedCount = maxedCount + 1 end
-
-        if (maxedCount == 3) then
             if (stopSkipped < maxStopSkip) then
                 stopSkipped = stopSkipped + 1
-                direction = 'up'
             else
                 stopSkipped = 0
                 direction = 'down'
@@ -68,19 +54,12 @@ local function PulseLED(type, speed, r, g, b, min)
         end
     end
 
-    local R = math.floor(savedR)
-    local G = math.floor(savedG)
-    local B = math.floor(savedB)
+    -- Apply brightness to maintain color ratio
+    local R = math.floor(savedBrightness * normR)
+    local G = math.floor(savedBrightness * normG)
+    local B = math.floor(savedBrightness * normB)
 
-    if (R >= 255) then R = 255 end
-    if (G >= 255) then G = 255 end
-    if (B >= 255) then B = 255 end
-    if (R <= 0) then R = 0 end
-    if (G <= 0) then G = 0 end
-    if (B <= 0) then B = 0 end
-    if (R <= min) then R = min end
-    if (G <= min) then G = min end
-    if (B <= min) then B = min end
+    -- Clamp values to the appropriate ranges
     if (R > r) then R = r end
     if (G > g) then G = g end
     if (B > b) then B = b end
